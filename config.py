f78f2f8e395d98de73982cb0924c9565d4befc68
ekanshgupta123 <ekanshgupta2003@gmail.com>
2022-12-02 12:46:48 -0800

    add files/database to app

---
+++ config.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__)) 
+app.config.from_mapping(
+    SECRET_KEY = 'CMPE-Group4',
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), nullable = False)
+    password = db.Column(db.String(200), nullable = False)
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
