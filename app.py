b5a1b5aba0e7b4fa6861aa29a422425eec1e5ee6
ekanshgupta123 <ekanshgupta2003@gmail.com>
2022-12-02 22:07:53 -0800

    delete account and alerts

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import login_user, current_user, logout_user, login_required
 
 from config import *
@@ -8,11 +8,7 @@ from signUp import signUp, loginForm
 def load_user(id):
     return User.query.get(int(id))
 
-
-@app.route('/')
-def homePage():
-    return render_template('homePage.html')
-
+@app.route('/', methods = ['GET', 'POST'])
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
     form = loginForm()
@@ -35,19 +31,28 @@ def createAccount():
         user = User(username=username, password=password)
         db.session.add(user)
         db.session.commit()
-        return redirect(url_for('login'))
+        return redirect(url_for('profile'))
     return render_template('signUp.html', form=form)
 
 @app.route('/profile', methods = ['GET', 'POST'])
 def profile():
     if not current_user.is_authenticated:
-        return "Please login before accessing"
+        return redirect(url_for('login'))
     return "Can only go here if logged in"
 
 @app.route('/logout', methods = ['GET'])
 def logout():
     logout_user()
-    return "Logged out!"
+    flash("You have been logged out. ")
+    return redirect(url_for('login'))
+
+@app.route('/delete', methods = ['GET', 'POST'])
+@login_required
+def delete():
+    current_user.delete()
+    db.session.commit()
+    flash('Your account has been successfully deleted.')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
