61f7d8c6f0ab391fb86fc3724aef0509de2dd794
ekanshgupta123 <ekanshgupta2003@gmail.com>
2022-12-02 16:22:35 -0800

    logged in, log out added

--- signUp.py
+++ signUp.py
@@ -2,17 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from config import User
+import re
 
 
-# def invalid_credentials(form, field):
-#     username_entered = form.username.data
-#     password_entered = field.data
+def invalid_credentials(form, field):
+    username_entered = form.username.data
+    password_entered = field.data
 
-#     user_object = User.query.filter_by(username=username_entered).first()
-#     if user_object is None:
-#         raise ValidationError("Username already exists. Select a different username. ")
-
-
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password is incorrect. ")
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or password is incorrect. ")
+    
 class signUp(FlaskForm):
     username = StringField('Username', validators=[DataRequired(message="Username required."),  
         Length(min=4, max=25, message="Username must be between 4 and 25 characters.")])
@@ -22,8 +24,26 @@ class signUp(FlaskForm):
         EqualTo('password', message='Password must match.')])
     submit = SubmitField('Sign Up')
 
+    #username cannot be in existing dataset
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('That username is taken. Please choose another.')
     
+    #password must have one special character and one number
+    def validate_password(self, password):
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+        numbers = ['0', '1', '2,', '3', '4', '5', '6', '7', '8', '9']
+        if regex.search(password.data) == None:
+            raise ValidationError('Password must have one special character and digit.')
+        else:
+            has_number = any([char in password.data for char in numbers])
+            print(has_number)
+            if (has_number == False):
+                raise ValidationError('Password must have one special character and digit.')
+    
+class loginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(message="Username required.")])
+    password = PasswordField('Password', validators=[DataRequired(message="Password required."), invalid_credentials])
+    rememberMe = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
